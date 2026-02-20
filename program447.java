a252267831b22b02e90e61c29c81f7c5ea35d48b
Pravin Chavan <pravinac8@gmail.com>
2026-02-20 13:19:19 +0530

    Add files via upload

---
+++ program447.java
@@ -0,0 +1,115 @@
+/*
+    Singly Circular Linked List
+    Function prototype only
+*/
+
+class node
+{
+    public int data;
+    public node next;
+
+    public node(int no)
+    {
+        this.data = no;
+        this.next = null;
+        
+    }
+}
+
+class SinglyCL
+{
+    private node first;
+    private node last;
+    private int iCount;
+
+    public SinglyCL()
+    {
+        System.out.println("Object of SinglyCL gets Created");
+        
+        this.first = null;
+        this.last = null;
+        this.iCount = 0;
+    }
+
+    public void InsertFirst(int no)
+    {}
+
+    public void InsertLast(int no)
+    {}
+
+    public void InsertAtPos(int no, int pos)
+    {}
+
+    public void DeleteFirst()
+    {}
+
+    public void DeleteLast()
+    {}
+
+    public void DeleteAtPos(int pos)
+    {}
+
+    public void Display()
+    {}
+
+    public int Count()
+    {
+        return this.iCount;
+    }
+}
+
+class program447
+{
+    public static void main(String A[]) 
+    {
+        SinglyCL obj = null;
+        int iRet = 0;
+
+        obj = new SinglyCL();
+
+        obj.InsertFirst(51);
+        obj.InsertFirst(21);
+        obj.InsertFirst(11);
+
+        obj.Display();
+        iRet = obj.Count();
+        System.out.println("Number of Nodes are : "+iRet);
+
+        obj.InsertLast(101);
+        obj.InsertLast(111);
+        obj.InsertLast(121);
+
+        obj.Display();
+        iRet = obj.Count();
+        System.out.println("Number of Nodes are : "+iRet);
+
+        obj.DeleteFirst();
+
+        obj.Display();
+        iRet = obj.Count();
+        System.out.println("Number of Nodes are : "+iRet);
+
+        obj.DeleteLast();
+
+        obj.Display();
+        iRet = obj.Count();
+        System.out.println("Number of Nodes are : "+iRet);
+
+        obj.InsertAtPos(105,4);
+
+        obj.Display();
+        iRet = obj.Count();
+        System.out.println("Number of Nodes are : "+iRet);
+
+        obj.DeleteAtPos(4);
+
+        obj.Display();
+        iRet = obj.Count();
+        System.out.println("Number of Nodes are : "+iRet);
+
+        // IMP for memory de-allocation
+        obj = null;
+        System.gc();
+    }
+    
+}
---
+++ program449.java
@@ -0,0 +1,117 @@
+/*
+    Doubly Circular Linked List
+    Function prototype only
+*/
+
+class node
+{
+    public int data;
+    public node next;
+    public node prev;
+
+    public node(int no)
+    {
+        this.data = no;
+        this.next = null;
+        this.prev = null;
+        
+    }
+}
+
+class DoublyCL
+{
+    private node first;
+    private node last;
+    private int iCount;
+
+    public DoublyCL()
+    {
+        System.out.println("Object of DoublyCLL gets Created");
+        
+        this.first = null;
+        this.last = null;
+        this.iCount = 0;
+    }
+
+    public void InsertFirst(int no)
+    {}
+
+    public void InsertLast(int no)
+    {}
+
+    public void InsertAtPos(int no, int pos)
+    {}
+
+    public void DeleteFirst()
+    {}
+
+    public void DeleteLast()
+    {}
+
+    public void DeleteAtPos(int pos)
+    {}
+
+    public void Display()
+    {}
+
+    public int Count()
+    {
+        return this.iCount;
+    }
+}
+
+class program449
+{
+    public static void main(String A[]) 
+    {
+        DoublyCL obj = null;
+        int iRet = 0;
+
+        obj = new DoublyCL();
+
+        obj.InsertFirst(51);
+        obj.InsertFirst(21);
+        obj.InsertFirst(11);
+
+        obj.Display();
+        iRet = obj.Count();
+        System.out.println("Number of Nodes are : "+iRet);
+
+        obj.InsertLast(101);
+        obj.InsertLast(111);
+        obj.InsertLast(121);
+
+        obj.Display();
+        iRet = obj.Count();
+        System.out.println("Number of Nodes are : "+iRet);
+
+        obj.DeleteFirst();
+
+        obj.Display();
+        iRet = obj.Count();
+        System.out.println("Number of Nodes are : "+iRet);
+
+        obj.DeleteLast();
+
+        obj.Display();
+        iRet = obj.Count();
+        System.out.println("Number of Nodes are : "+iRet);
+
+        obj.InsertAtPos(105,4);
+
+        obj.Display();
+        iRet = obj.Count();
+        System.out.println("Number of Nodes are : "+iRet);
+
+        obj.DeleteAtPos(4);
+
+        obj.Display();
+        iRet = obj.Count();
+        System.out.println("Number of Nodes are : "+iRet);
+
+        // IMP for memory de-allocation
+        obj = null;
+        System.gc();
+    }
+    
+}
